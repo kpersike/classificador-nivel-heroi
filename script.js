37c1033a9a980dca624a1151ebcc268ff3f30370
kpersike <kaikpersikemai@gmail.com>
2025-07-16 19:32:49 -0300

    feat: versao final

---
+++ script.js
@@ -0,0 +1,39 @@
+/*
+    **Challenge 1 - Hero Level
+
+    Package installed: prompt-sync
+        - Command to install: npm install prompt-sync
+
+    @kpersike
+*/
+
+const prompt = require('prompt-sync')();
+
+let nome = "";
+let xp = null;
+let nivel = "";
+
+do{
+    nome = prompt("Informe o nome ");
+    xp = parseInt(prompt("Quantidade de experiencia (XP) "));
+}while(nome === "" || isNaN(xp));
+
+if(xp < 1000){
+    nivel = "Ferro";
+}else if(xp >= 1001 && xp <= 2000){
+    nivel = "Bronze";
+}else if(xp >= 2001 && xp <= 5000){
+    nivel = "Prata";
+}else if(xp >= 5001 && xp <= 7000){
+    nivel = "Ouro";
+}else if(xp >= 7001 && xp <= 8000){
+    nivel = "Platina";
+}else if(xp >= 8001 && xp <= 9000){
+    nivel = "Ascendente";
+}else if(xp >= 9001 && xp <= 10000){
+    nivel = "Imortal";
+}else if(xp >= 10001){
+    nivel = "Radiante";
+}
+
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
